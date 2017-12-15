0a0eaa7e09340f3d93f5d70760c6ccdccc1240d9
gogoforit <715157026@qq.com>
2017-12-15 22:18:11 +0800

    Modify setup.py, the version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bilibili-voice',
-    version='0.1.0.0',
+    version='0.1.0.1',
     packages=find_packages(),
     install_requires=[
         'future',
