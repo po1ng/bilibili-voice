2fb9d31b286fb8567d64183a071b013c1cadfd43
gogoforit <715157026@qq.com>
2017-12-17 10:10:31 +0800

    Version 0.1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bilibili-voice',
-    version='0.1.0.1',
+    version='0.1.1.0',
     packages=find_packages(),
     install_requires=[
         'future',
