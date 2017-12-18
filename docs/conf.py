06bbfffedbe06bcc25e887c3b4b0187df6799ef1
ShiXian Wan <715157026@qq.com>
2017-12-18 20:44:50 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-# import bilibilivoice
+import bilibilivoice
 
 # -- General configuration ------------------------------------------------
 
