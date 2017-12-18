21f86843fde8465df01553d8925e9a7d41689a9a
ShiXian Wan <715157026@qq.com>
2017-12-18 20:01:41 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,6 +21,8 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
+import bilibilivoice
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
